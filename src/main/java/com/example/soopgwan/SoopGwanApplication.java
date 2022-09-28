4cf547d5095f5486ced228c5b4f64fe3728a1a98
anminwoo <minwoo5995@gmail.com>
2022-09-28 20:12:25 +0900

    🎉 :: project setting

---
+++ src/main/java/com/example/soopgwan/SoopGwanApplication.java
@@ -0,0 +1,13 @@
+package com.example.soopgwan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoopGwanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoopGwanApplication.class, args);
+    }
+
+}

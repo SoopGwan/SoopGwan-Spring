87d1f0c8984a8c8a75e0d2bedd4dc482d5b71db5
anminwoo <minwoo5995@gmail.com>
2022-10-18 16:22:31 +0900

    📑 :: 요일 엔티티 추가

---
+++ src/main/java/com/example/soopgwan/domain/day/domain/Day.java
@@ -0,0 +1,28 @@
+package com.example.soopgwan.domain.day.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Table(name = "tbl_day")
+@Entity
+public class Day {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "BIGINT", nullable = false)
+    private Long id;
+
+    @Column(columnDefinition = "DATETIME", nullable = false)
+    private LocalDateTime startAt;
+
+    @Column(columnDefinition = "DATETIME", nullable = false)
+    private LocalDateTime endAt;
+
+    @Column(columnDefinition = "BIGINT", nullable = false)
+    private LocalDateTime eval;
+}

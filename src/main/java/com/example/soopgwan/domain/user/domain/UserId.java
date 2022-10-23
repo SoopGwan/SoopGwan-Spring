1a3c2abbc204e51280f3b8d712066c5aa941e7a7
anminwoo <minwoo5995@gmail.com>
2022-10-23 23:00:08 +0900

    📑 :: 유저아이디 위치 변경

--- src/main/java/com/example/soopgwan/domain/user/domain/UserId.java
+++ src/main/java/com/example/soopgwan/domain/user/domain/UserId.java
@@ -1,13 +1,16 @@
-package com.example.soopgwan.domain.achivesuccess.domain;
+package com.example.soopgwan.domain.user.domain;
 
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import java.io.Serializable;
 
+@Getter
 @Embeddable
-@NoArgsConstructor
+@EqualsAndHashCode
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserId implements Serializable {
 
     @Column(columnDefinition = "BIGINT", nullable = false)

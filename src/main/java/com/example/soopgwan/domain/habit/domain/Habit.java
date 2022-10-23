541ff2a81e3734be9566cfef9dea12cd3605259d
anminwoo <minwoo5995@gmail.com>
2022-10-23 22:56:54 +0900

    📝 :: 습관 엔티티 수정

--- src/main/java/com/example/soopgwan/domain/habit/domain/Habit.java
+++ src/main/java/com/example/soopgwan/domain/habit/domain/Habit.java
@@ -1,31 +1,29 @@
 package com.example.soopgwan.domain.habit.domain;
 
 import lombok.*;
-import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.time.LocalDate;
 
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
 @Table(name = "tbl_habit")
 @Entity
 public class Habit {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(columnDefinition = "BIGINT", nullable = false)
     private long id;
 
     @Column(columnDefinition = "VARCHAR(200)", nullable = false)
     private String content;
 
-    @DateTimeFormat(pattern = "yyyy-MM--dd")
     @Column(columnDefinition = "DATETIME", nullable = false)
     private LocalDate startAt;
 
-    @DateTimeFormat(pattern = "yyyy-MM--dd")
     @Column(columnDefinition = "DATETIME", nullable = false)
     private LocalDate endAt;
 

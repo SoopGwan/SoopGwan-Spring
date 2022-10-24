2f8b5e20031df96b15f813e88f8e1bd55f163f41
정대현 <eogus_2513@dsm.hs.kr>
2022-10-24 23:40:42 +0900

    📑 :: BaseTimeEntity

---
+++ src/main/java/com/example/soopgwan/global/entity/BaseTimeEntity.java
@@ -0,0 +1,24 @@
+package com.example.soopgwan.global.entity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor
+@SuperBuilder
+@MappedSuperclass
+public abstract class BaseTimeEntity {
+
+    @Column(nullable = false)
+    private LocalDate startAt;
+
+    @Column(nullable = false)
+    private LocalDate endAt;
+}

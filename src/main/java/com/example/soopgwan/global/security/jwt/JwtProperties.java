6a190d40c83be9906a840e2d70b1b2cb0eb64834
anminwoo <minwoo5995@gmail.com>
2022-10-28 16:13:31 +0900

    📑 :: JwtProperties 생성

---
+++ src/main/java/com/example/soopgwan/global/security/jwt/JwtProperties.java
@@ -0,0 +1,21 @@
+package com.example.soopgwan.global.security.jwt;
+
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+import java.util.Base64;
+
+@Getter
+@ConstructorBinding
+@ConfigurationProperties(prefix = "jwt")
+public class JwtProperties {
+
+    private final String SecretKey;
+    private final Long accessExp;
+
+    public JwtProperties(String secretKey, long accessExp) {
+        this.SecretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+        this.accessExp = accessExp;
+    }
+}

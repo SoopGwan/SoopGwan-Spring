43b2e4029df5e253ff4d5657360f897684473429
anminwoo <minwoo5995@gmail.com>
2022-10-28 16:12:43 +0900

    ♻️ :: ErrorCode 추가

--- src/main/java/com/example/soopgwan/global/error/ErrorCode.java
+++ src/main/java/com/example/soopgwan/global/error/ErrorCode.java
@@ -8,6 +8,12 @@ import lombok.Getter;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum ErrorCode {
 
+    BAD_REQUEST(400, "Request가 잘못됨"),
+
+    NOT_FOUND(404, "해당 id가 존재하지 않음"),
+    HABIT_DELETE_NOT_FOUND(404, "해당 id가 존재하지 않음"),
+    HABIT_SUCCESS_NOT_FOUND(404, "해당 id 습관이 없음"),
+    HABIT_NOT_FOUND(404, "habit id가 없음"),
     INTERNAL_SERVER_ERROR(500, "서버 에러");
 
     private final int statusCode;

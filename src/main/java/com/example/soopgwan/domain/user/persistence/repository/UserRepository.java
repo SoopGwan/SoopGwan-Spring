a1ced95ce40bc4de6358ecf533d52ec08e540318
anminwoo <minwoo5995@gmail.com>
2022-10-29 12:06:17 +0900

    ♻️ :: UserRepository 수정

--- src/main/java/com/example/soopgwan/domain/user/persistence/repository/UserRepository.java
+++ src/main/java/com/example/soopgwan/domain/user/persistence/repository/UserRepository.java
@@ -4,6 +4,12 @@ import com.example.soopgwan.domain.user.persistence.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByAccountId(String accountId);
+
+    Boolean existsByAccountId(String accountId);
 }

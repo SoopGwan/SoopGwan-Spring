5c70dc0c1e82ba1fe72e6a2f3b14fce0566a46ee
anminwoo <minwoo5995@gmail.com>
2022-10-29 12:28:12 +0900

    📑 :: UserNotFound 생성

---
+++ src/main/java/com/example/soopgwan/global/security/exception/UserNotFound.java
@@ -0,0 +1,13 @@
+package com.example.soopgwan.global.security.exception;
+
+import com.example.soopgwan.global.error.CustomException;
+import com.example.soopgwan.global.error.ErrorCode;
+
+public class UserNotFound extends CustomException {
+
+    public static final CustomException EXCEPTION = new UserNotFound();
+
+    private UserNotFound() {
+        super(ErrorCode.USER_NOT_FOUND);
+    }
+}

ce911462d6454d3f09b2574e4e39df539646825e
anminwoo <minwoo5995@gmail.com>
2022-10-31 23:11:14 +0900

    ♻️ :: 줄 바꿈, createHabit 어노테이션 추가

--- src/main/java/com/example/soopgwan/domain/habit/presentation/HabitController.java
+++ src/main/java/com/example/soopgwan/domain/habit/presentation/HabitController.java
@@ -4,20 +4,20 @@ import com.example.soopgwan.domain.habit.application.HabitService;
 import com.example.soopgwan.domain.habit.presentation.dto.request.CreateHabitRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RequestMapping("/habit")
 @RestController
 public class HabitController {
+
     private final HabitService habitService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public void createHabit(CreateHabitRequest request) {
+    public void createHabit(@Valid @RequestBody CreateHabitRequest request) {
         habitService.createHabit(request);
     }
 }

d29df4d2f22f4bedfdaf34306fb90722e1b6c95a
anminwoo <minwoo5995@gmail.com>
2022-10-31 22:33:41 +0900

    📑 :: CreateHabitRequest 생성

---
+++ src/main/java/com/example/soopgwan/domain/habit/presentation/dto/request/CreateHabitRequest.java
@@ -0,0 +1,11 @@
+package com.example.soopgwan.domain.habit.presentation.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class CreateHabitRequest {
+
+    private String content;
+}

627d9e826c931296f0d317e0669b55b56a953e6e
anminwoo <minwoo5995@gmail.com>
2022-10-31 23:10:36 +0900

    ♻️ :: content에 notblank 추가

--- src/main/java/com/example/soopgwan/domain/habit/presentation/dto/request/CreateHabitRequest.java
+++ src/main/java/com/example/soopgwan/domain/habit/presentation/dto/request/CreateHabitRequest.java
@@ -3,9 +3,12 @@ package com.example.soopgwan.domain.habit.presentation.dto.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @NoArgsConstructor
 public class CreateHabitRequest {
 
+    @NotBlank
     private String content;
 }

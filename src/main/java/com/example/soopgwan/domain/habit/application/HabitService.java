045ce04114892e39ec991cc0a7aade83524b552c
anminwoo <minwoo5995@gmail.com>
2022-11-01 21:23:17 +0900

    ♻️ :: HabitService 수정

--- src/main/java/com/example/soopgwan/domain/habit/application/HabitService.java
+++ src/main/java/com/example/soopgwan/domain/habit/application/HabitService.java
@@ -1,15 +1,14 @@
 package com.example.soopgwan.domain.habit.application;
 
+import com.example.soopgwan.domain.habit.exception.HabitNotFound;
 import com.example.soopgwan.domain.habit.persistence.WeekHabit;
 import com.example.soopgwan.domain.habit.persistence.repository.WeekHabitRepository;
 import com.example.soopgwan.domain.habit.presentation.dto.request.CreateHabitRequest;
-import com.example.soopgwan.domain.user.application.UserService;
 import com.example.soopgwan.domain.user.persistence.User;
 import com.example.soopgwan.global.util.UserUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
 import java.time.LocalDate;
 
 @RequiredArgsConstructor
@@ -31,5 +30,12 @@ public class HabitService {
                 .build();
 
         weekHabitRepository.save(weekHabit);
+    }
+
+    public void deleteHabit(Long habitId) {
+        WeekHabit weekHabit = weekHabitRepository.findById(habitId)
+                .orElseThrow(() -> HabitNotFound.EXCEPTION);
+
+        weekHabitRepository.delete(weekHabit);
     }
 }

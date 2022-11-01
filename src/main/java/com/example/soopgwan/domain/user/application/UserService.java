f822dd6a86a2bcbeaf7a12d224e7f15625d68adf
anminwoo <minwoo5995@gmail.com>
2022-11-01 16:25:44 +0900

    ♻️ :: UserService 수정

--- src/main/java/com/example/soopgwan/domain/user/application/UserService.java
+++ src/main/java/com/example/soopgwan/domain/user/application/UserService.java
@@ -3,6 +3,7 @@ package com.example.soopgwan.domain.user.application;
 import com.example.soopgwan.domain.user.persistence.User;
 import com.example.soopgwan.domain.user.persistence.repository.UserRepository;
 import com.example.soopgwan.global.exception.UserNotFound;
+import com.example.soopgwan.global.util.UserUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -10,11 +11,6 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Service
 public class UserService {
+    
     private final UserRepository userRepository;
-
-    public User getCurrentUser() {
-        String id = SecurityContextHolder.getContext().getAuthentication().getName();
-        return userRepository.findByAccountId(id)
-                .orElseThrow(() -> UserNotFound.EXCEPTION);
-    }
 }

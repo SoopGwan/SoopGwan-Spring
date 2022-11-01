c18191c84d969795325425e57cc2646ea1164935
anminwoo <minwoo5995@gmail.com>
2022-11-01 21:25:30 +0900

    ♻️ :: 줄바꿈

--- src/main/java/com/example/soopgwan/global/util/UserUtil.java
+++ src/main/java/com/example/soopgwan/global/util/UserUtil.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Service
 public class UserUtil {
-    
+
     private final UserRepository userRepository;
 
     public User getCurrentUser() {

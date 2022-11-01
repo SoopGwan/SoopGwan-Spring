f1679425b66dcfd0d70858f98b985ffe12fd6e2d
anminwoo <minwoo5995@gmail.com>
2022-11-01 21:24:58 +0900

    ♻️ :: 사용하지 않는 import 삭제

--- src/main/java/com/example/soopgwan/domain/user/application/UserService.java
+++ src/main/java/com/example/soopgwan/domain/user/application/UserService.java
@@ -1,11 +1,7 @@
 package com.example.soopgwan.domain.user.application;
 
-import com.example.soopgwan.domain.user.persistence.User;
 import com.example.soopgwan.domain.user.persistence.repository.UserRepository;
-import com.example.soopgwan.global.exception.UserNotFound;
-import com.example.soopgwan.global.util.UserUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor

51bf032b23960d369d19dc6b9cbb30a2390efcd3
anminwoo <minwoo5995@gmail.com>
2022-11-02 19:09:45 +0900

    ♻️ :: 일주일치 습관 실천 체크 API 추가

--- src/main/java/com/example/soopgwan/domain/habit/persistence/repository/WeekHabitRepository.java
+++ src/main/java/com/example/soopgwan/domain/habit/persistence/repository/WeekHabitRepository.java
@@ -4,6 +4,11 @@ import com.example.soopgwan.domain.habit.persistence.WeekHabit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface WeekHabitRepository extends JpaRepository<WeekHabit, Long> {
+
+    List<WeekHabit> findAllByStartAtBetween(LocalDate startAt, LocalDate endAt);
 }

836ed7962c60114cbcb2ad349e4d76fd7ade6898
정대현 <eogus_2513@dsm.hs.kr>
2022-11-02 11:40:20 +0900

    ♻️:: findByWeekHabit() 추가

--- src/main/java/com/example/soopgwan/domain/habit/persistence/repository/HabitSuccessRepository.java
+++ src/main/java/com/example/soopgwan/domain/habit/persistence/repository/HabitSuccessRepository.java
@@ -1,9 +1,14 @@
 package com.example.soopgwan.domain.habit.persistence.repository;
 
 import com.example.soopgwan.domain.habit.persistence.HabitSuccess;
+import com.example.soopgwan.domain.habit.persistence.WeekHabit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface HabitSuccessRepository extends JpaRepository<HabitSuccess, Long> {
+
+    Optional<HabitSuccess> findByWeekHabit(WeekHabit weekHabit);
 }

b8d79bd4334480ce742bf27b5ad8942c878277a5
정대현 <eogus_2513@dsm.hs.kr>
2022-11-02 11:40:44 +0900

    ♻️:: 형식 오류 수정

--- src/main/java/com/example/soopgwan/domain/habit/presentation/HabitController.java
+++ src/main/java/com/example/soopgwan/domain/habit/presentation/HabitController.java
@@ -4,7 +4,14 @@ import com.example.soopgwan.domain.habit.application.HabitService;
 import com.example.soopgwan.domain.habit.presentation.dto.request.CreateHabitRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
@@ -22,13 +29,12 @@ public class HabitController {
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @DeleteMapping("{habit-id}")
+    @DeleteMapping("/{habit-id}")
     public void deleteHabit(@PathVariable("habit-id") Long habitId) {
         habitService.deleteHabit(habitId);
     }
 
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    @PatchMapping("{habit-id}")
+    @PatchMapping("/{habit-id}")
     public void checkHabitSuccess(@PathVariable("habit-id") Long habitId) {
         habitService.checkHabitSuccess(habitId);
     }

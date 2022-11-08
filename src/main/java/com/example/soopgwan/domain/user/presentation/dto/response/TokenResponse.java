1ebdf59615dadbee7feaba8d6b2b0c7c2e10eaa7
태라 <terra2007@naver.com>
2022-11-08 20:31:11 +0900

    📑 :: Token Response 생성

---
+++ src/main/java/com/example/soopgwan/domain/user/presentation/dto/response/TokenResponse.java
@@ -0,0 +1,11 @@
+package com.example.soopgwan.domain.user.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TokenResponse {
+    private final String accessToken;
+    private final String refreshToken;
+}

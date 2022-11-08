a51a333bd481a1a6b1ba9ed75c4c0310a1beca3c
태라 <terra2007@naver.com>
2022-11-08 20:30:22 +0900

    📑 :: UserService 수정

--- src/main/java/com/example/soopgwan/domain/user/application/UserService.java
+++ src/main/java/com/example/soopgwan/domain/user/application/UserService.java
@@ -1,7 +1,13 @@
 package com.example.soopgwan.domain.user.application;
 
+import com.example.soopgwan.domain.user.exception.UserExistsException;
+import com.example.soopgwan.domain.user.persistence.User;
 import com.example.soopgwan.domain.user.persistence.repository.UserRepository;
+import com.example.soopgwan.domain.user.presentation.dto.request.SignUpRequset;
+import com.example.soopgwan.domain.user.presentation.dto.response.TokenResponse;
+import com.example.soopgwan.global.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -9,4 +15,25 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public TokenResponse execute(SignUpRequset request) {
+        if (userRepository.findByAccountId(request.getAccountId()).isPresent()) {
+            throw UserExistsException.EXCEPTION;
+        }
+
+        User user = User.builder()
+                .accountId(request.getAccountId())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .phoneNumber(request.getPhoneNumber())
+                .build();
+        userRepository.save(user);
+
+        String accessToken = jwtTokenProvider.generateToken(user.getAccountId(), "access");
+        String refreshToken = jwtTokenProvider.generateToken(user.getAccountId(), "refresh");
+        return new TokenResponse(accessToken, refreshToken);
+    }
 }

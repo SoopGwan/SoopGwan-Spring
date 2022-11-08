0f1b2b0a164d21d4778bc4b0b6b210dff99e4fa3
태라 <terra2007@naver.com>
2022-11-09 08:23:49 +0900

    ♻️ :: TokenResonse 리팩토링

--- src/main/java/com/example/soopgwan/domain/user/presentation/dto/response/TokenResponse.java
+++ src/main/java/com/example/soopgwan/domain/user/presentation/dto/response/TokenResponse.java
@@ -2,9 +2,11 @@ package com.example.soopgwan.domain.user.presentation.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@RequiredArgsConstructor
 public class TokenResponse {
     private final String accessToken;
     private final String refreshToken;

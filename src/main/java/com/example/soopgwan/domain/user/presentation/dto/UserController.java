e6f35fed47cd079c391b1b55caacb529d861de5f
태라 <terra2007@naver.com>
2022-11-08 20:30:51 +0900

    📑 :: UserController 생성

---
+++ src/main/java/com/example/soopgwan/domain/user/presentation/dto/UserController.java
@@ -0,0 +1,25 @@
+package com.example.soopgwan.domain.user.presentation.dto;
+
+import com.example.soopgwan.domain.user.application.UserService;
+import com.example.soopgwan.domain.user.presentation.dto.request.SignUpRequset;
+import com.example.soopgwan.domain.user.presentation.dto.response.TokenResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+
+@RequiredArgsConstructor
+@RequestMapping("/users")
+@RestController
+public class UserController {
+    private final UserService signUpService;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/signup")
+    public TokenResponse signUp(@RequestBody @Valid SignUpRequset request){
+        return signUpService.execute(request);
+    }
+}

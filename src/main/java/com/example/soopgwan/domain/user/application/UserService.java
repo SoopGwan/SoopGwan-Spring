86b2cfaaed98fd1e946dab62ef77e8461eca9ced
태라 <terra2007@naver.com>
2022-11-09 11:29:00 +0900

    ⚡️ :: changePassword UserService 생성

--- src/main/java/com/example/soopgwan/domain/user/application/UserService.java
+++ src/main/java/com/example/soopgwan/domain/user/application/UserService.java
@@ -1,17 +1,23 @@
 package com.example.soopgwan.domain.user.application;
 
+import com.example.soopgwan.domain.user.exception.PasswordDifferent;
 import com.example.soopgwan.domain.user.exception.PasswordMisMatch;
 import com.example.soopgwan.domain.user.exception.UserExists;
 import com.example.soopgwan.domain.user.exception.UserNotFound;
 import com.example.soopgwan.domain.user.persistence.User;
 import com.example.soopgwan.domain.user.persistence.repository.UserRepository;
+import com.example.soopgwan.domain.user.presentation.dto.request.ChangePasswordRequest;
 import com.example.soopgwan.domain.user.presentation.dto.request.LoginRequest;
 import com.example.soopgwan.domain.user.presentation.dto.request.SignUpRequset;
 import com.example.soopgwan.domain.user.presentation.dto.response.TokenResponse;
 import com.example.soopgwan.global.security.jwt.JwtTokenProvider;
+import com.example.soopgwan.global.util.UserUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Objects;
 
 @RequiredArgsConstructor
 @Service
@@ -20,6 +26,7 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtTokenProvider jwtTokenProvider;
+    private final UserUtil userUtil;
 
     public TokenResponse signUp(SignUpRequset request) {
         if (userRepository.findByAccountId(request.getAccountId()).isPresent()) {
@@ -55,5 +62,16 @@ public class UserService {
         if (userRepository.findByAccountId(accountId).isPresent()) {
             throw UserExists.EXCEPTION;
         }
+    }
+
+    @Transactional()
+    public void changePassword(ChangePasswordRequest request) {
+        if (!request.getPassword().equals(request.getRepeatPassword())) {
+            throw PasswordDifferent.EXCEPTION;
+        }
+
+        User user = userUtil.getCurrentUser();
+
+        user.changePassword(passwordEncoder.encode(request.getPassword()));
     }
 }

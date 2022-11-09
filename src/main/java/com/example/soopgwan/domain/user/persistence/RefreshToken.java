661b63bfac8f328eed4f1aba0ffa16e4ef16b095
태라 <terra2007@naver.com>
2022-11-09 15:38:45 +0900

    📑 :: RefreshToken 생성

---
+++ src/main/java/com/example/soopgwan/domain/user/persistence/RefreshToken.java
@@ -0,0 +1,32 @@
+package com.example.soopgwan.domain.user.persistence;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+import org.springframework.data.redis.core.index.Indexed;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@RedisHash
+@Builder
+public class RefreshToken {
+
+    @Id
+    private String accountId;
+
+    @Indexed
+    private String refreshToken;
+
+    @TimeToLive
+    private Long ttl;
+
+    public void updateToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+}

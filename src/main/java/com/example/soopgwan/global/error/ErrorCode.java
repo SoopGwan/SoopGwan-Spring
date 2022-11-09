307992f4abef8dd657a5b4157893e2e36b3776a5
태라 <terra2007@naver.com>
2022-11-09 11:26:36 +0900

    ⚡️ :: ErrorCode 추가

--- src/main/java/com/example/soopgwan/global/error/ErrorCode.java
+++ src/main/java/com/example/soopgwan/global/error/ErrorCode.java
@@ -11,6 +11,7 @@ public enum ErrorCode {
     EXPIRED_TOKEN(401, "토큰 만료"),
     INVALID_TOKEN(401, "잘못된 토큰"),
     PASSWORD_MISMATCH(401, "비밀번호가 잘못됨"),
+    PASSWORD_DIFFERENT(401, "비밀번호가 일치하지 않음"),
 
     USER_NOT_FOUND(404, "유저를 찾을 수 없음"),
     HABIT_NOT_FOUND(404, "습관을 찾을 수 없음"),

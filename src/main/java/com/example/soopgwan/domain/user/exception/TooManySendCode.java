72284450e3bff64217ec8822c1136e682b23ef9d
태라 <terra2007@naver.com>
2022-11-10 07:49:35 +0900

    📑 :: TooManySendCode Exception 추가

---
+++ src/main/java/com/example/soopgwan/domain/user/exception/TooManySendCode.java
@@ -0,0 +1,14 @@
+package com.example.soopgwan.domain.user.exception;
+
+import com.example.soopgwan.global.error.CustomException;
+import com.example.soopgwan.global.error.ErrorCode;
+
+public class TooManySendCode extends CustomException {
+
+    public static final TooManySendCode EXCEPTION =
+            new TooManySendCode();
+
+    private TooManySendCode() {
+        super(ErrorCode.TOO_MANY_SEND_CODE);
+    }
+}

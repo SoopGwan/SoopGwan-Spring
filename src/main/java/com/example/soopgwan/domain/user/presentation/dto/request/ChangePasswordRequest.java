ad99f4ce6269899c2680883e4aadd9865d89631c
태라 <terra2007@naver.com>
2022-11-09 11:27:40 +0900

    📑 :: ChangePassword DTO 생성

---
+++ src/main/java/com/example/soopgwan/domain/user/presentation/dto/request/ChangePasswordRequest.java
@@ -0,0 +1,20 @@
+package com.example.soopgwan.domain.user.presentation.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Getter
+@NoArgsConstructor
+public class ChangePasswordRequest {
+
+    @NotBlank
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,24}$",
+            message = "password는 영문자[a-z | A-Z], 숫자[0-9], 특수문자[!, @, #, $, %, ^, &, +, =]가 최소 1개씩 포함되어야 합니다.")
+    private String password;
+
+    @NotBlank
+    private String repeatPassword;
+}

a039f7a17bb926d84a99a7e8e2efe67f8d2e1ba0
태라 <terra2007@naver.com>
2022-11-10 07:48:56 +0900

    📑 :: SendCodeRequest DTO 생성

---
+++ src/main/java/com/example/soopgwan/domain/user/presentation/dto/request/SendCodeRequest.java
@@ -0,0 +1,16 @@
+package com.example.soopgwan.domain.user.presentation.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Getter
+@NoArgsConstructor
+public class SendCodeRequest {
+
+    @NotBlank
+    @Size(min = 11, max = 11)
+    private String phoneNumber;
+}

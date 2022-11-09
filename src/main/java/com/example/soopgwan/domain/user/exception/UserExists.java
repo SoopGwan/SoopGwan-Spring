4c4d8df93ab6fa1daa1661265ee1ed2426bb6853
태라 <terra2007@naver.com>
2022-11-09 09:52:12 +0900

    ♻️ :: 클래스 이름 리팩토링

--- src/main/java/com/example/soopgwan/domain/user/exception/UserExists.java
+++ src/main/java/com/example/soopgwan/domain/user/exception/UserExists.java
@@ -3,12 +3,12 @@ package com.example.soopgwan.domain.user.exception;
 import com.example.soopgwan.global.error.CustomException;
 import com.example.soopgwan.global.error.ErrorCode;
 
-public class UserExistsException extends CustomException {
+public class UserExists extends CustomException {
 
     public static final CustomException EXCEPTION =
-            new UserExistsException();
+            new UserExists();
 
-    private UserExistsException() {
+    private UserExists() {
         super(ErrorCode.USER_EXISTS);
     }
 }

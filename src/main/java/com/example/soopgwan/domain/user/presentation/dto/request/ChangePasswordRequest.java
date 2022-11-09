bb984aa8513629f9203da56b236057f30dbef6dd
태라 <terra2007@naver.com>
2022-11-09 11:36:22 +0900

    ⚡️ :: 비밀번호 정규식 추가

--- src/main/java/com/example/soopgwan/domain/user/presentation/dto/request/ChangePasswordRequest.java
+++ src/main/java/com/example/soopgwan/domain/user/presentation/dto/request/ChangePasswordRequest.java
@@ -16,5 +16,7 @@ public class ChangePasswordRequest {
     private String password;
 
     @NotBlank
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,24}$",
+            message = "password는 영문자[a-z | A-Z], 숫자[0-9], 특수문자[!, @, #, $, %, ^, &, +, =]가 최소 1개씩 포함되어야 합니다.")
     private String repeatPassword;
 }

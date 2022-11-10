2b181bd8f61e72ed9231f97feacce89400c96a1b
태라 <terra2007@naver.com>
2022-11-10 17:24:04 +0900

    ⚡️ :: VerifyCode Service 추가

--- src/main/java/com/example/soopgwan/domain/user/application/UserService.java
+++ src/main/java/com/example/soopgwan/domain/user/application/UserService.java
@@ -1,17 +1,11 @@
 package com.example.soopgwan.domain.user.application;
 
-import com.example.soopgwan.domain.user.exception.PasswordDifferent;
-import com.example.soopgwan.domain.user.exception.PasswordMisMatch;
-import com.example.soopgwan.domain.user.exception.TooManySendCode;
-import com.example.soopgwan.domain.user.exception.UserExists;
-import com.example.soopgwan.domain.user.exception.UserNotFound;
+import com.example.soopgwan.domain.user.exception.*;
 import com.example.soopgwan.domain.user.persistence.User;
+import com.example.soopgwan.domain.user.persistence.VerifyCode;
 import com.example.soopgwan.domain.user.persistence.repository.UserRepository;
 import com.example.soopgwan.domain.user.persistence.repository.VerifyCodeRepository;
-import com.example.soopgwan.domain.user.presentation.dto.request.ChangePasswordRequest;
-import com.example.soopgwan.domain.user.presentation.dto.request.LoginRequest;
-import com.example.soopgwan.domain.user.presentation.dto.request.SendCodeRequest;
-import com.example.soopgwan.domain.user.presentation.dto.request.SignUpRequset;
+import com.example.soopgwan.domain.user.presentation.dto.request.*;
 import com.example.soopgwan.domain.user.presentation.dto.response.TokenResponse;
 import com.example.soopgwan.global.security.jwt.JwtTokenProvider;
 import com.example.soopgwan.global.util.UserUtil;
@@ -21,6 +15,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Objects;
 
 @RequiredArgsConstructor
 @Service
@@ -90,5 +85,14 @@ public class UserService {
         }
 
         messageUtil.send(request.getPhoneNumber(), count);
+    }
+
+    public void verifyCode(VerifyCodeRequest request) {
+        VerifyCode verifyCode = verifyCodeRepository.findById(request.getPhoneNumber())
+                .orElseThrow(() -> VerifyCodeDifferent.EXCEPTION);
+
+        if (!verifyCode.getCode().equals(request.getCode())) {
+            throw VerifyCodeDifferent.EXCEPTION;
+        }
     }
 }

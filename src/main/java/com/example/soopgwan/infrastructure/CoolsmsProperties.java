f3b484d56fbad620c02e6fda0fe8f8cfeae7b995
태라 <terra2007@naver.com>
2022-11-10 12:11:51 +0900

    ♻️ :: RequiredArgsConstructor 사용

--- src/main/java/com/example/soopgwan/infrastructure/CoolsmsProperties.java
+++ src/main/java/com/example/soopgwan/infrastructure/CoolsmsProperties.java
@@ -1,21 +1,17 @@
 package com.example.soopgwan.infrastructure;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 
 @Getter
 @ConstructorBinding
 @ConfigurationProperties(prefix = "coolsms")
+@RequiredArgsConstructor
 public class CoolsmsProperties {
 
     private final String apiKey;
     private final String apiSecret;
     private final String sender;
-
-    public CoolsmsProperties(String apiKey, String apiSecret, String sender) {
-        this.apiKey = apiKey;
-        this.apiSecret = apiSecret;
-        this.sender = sender;
-    }
 }

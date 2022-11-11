e0fea6e59d4c6f755fd794479081faa07dce25f4
태라 <terra2007@naver.com>
2022-11-11 10:55:27 +0900

    📑 :: ResetPasswordResponse DTO 생성

---
+++ src/main/java/com/example/soopgwan/domain/user/presentation/dto/response/ResetPasswordResponse.java
@@ -0,0 +1,11 @@
+package com.example.soopgwan.domain.user.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ResetPasswordResponse {
+
+    private final String password;
+}

be59473bde1c0ace643efe1bf36fb7118cae22c5
정대현 <eogus_2513@dsm.hs.kr>
2022-11-24 19:48:46 +0900

    🔀 :: show sql 설정

--- src/main/java/com/example/soopgwan/domain/habit/persistence/repository/WeekHabitRepositoryCustomImpl.java
+++ src/main/java/com/example/soopgwan/domain/habit/persistence/repository/WeekHabitRepositoryCustomImpl.java
@@ -3,8 +3,6 @@ package com.example.soopgwan.domain.habit.persistence.repository;
 import com.example.soopgwan.domain.habit.persistence.repository.vo.QWeekHabitVO;
 import com.example.soopgwan.domain.habit.persistence.repository.vo.WeekHabitVO;
 import com.example.soopgwan.domain.user.persistence.User;
-import com.querydsl.jpa.JPAExpressions;
-import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
@@ -12,7 +10,6 @@ import java.time.LocalDate;
 import java.util.List;
 
 import static com.example.soopgwan.domain.habit.persistence.QWeekHabit.weekHabit;
-import static com.example.soopgwan.domain.habit.persistence.QWeekHabitStatus.weekHabitStatus;
 
 @RequiredArgsConstructor
 public class WeekHabitRepositoryCustomImpl implements WeekHabitRepositoryCustom {
